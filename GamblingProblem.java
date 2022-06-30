7064dedb7dec525354dc239057b82f2d0b547a02
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-07-01 00:40:20 +0530

    Calculate total amount won after 20 days

--- GamblingProblem.java
+++ GamblingProblem.java
@@ -1,33 +1,41 @@
 public class GamblingProblem {
-    public static void main(String[] args) {
-        int everydayStake = 100;
-        int everygameBet = 1;
-        int cashInHand = everydayStake;
-        while (cashInHand > 50 && cashInHand < 150) {
-            int gamble = (int) Math.floor(Math.random() * 10) % 2;
+    static int everydayStake=100;
+    static int everygameBet=1;
+    static int finalAmount=0;
+    
+    public static void main(String[] args){
+        for (int i=1; i<=20; i++){
+            int cashInHand = everydayStake;
+            while (cashInHand > 50 && cashInHand < 150) {
+                int gamble = (int) Math.floor(Math.random() * 10) % 2;
 
-            switch (gamble) {
-                case 1:
-                    System.out.println("You have won");
-                    cashInHand = cashInHand + everygameBet;
-                    System.out.println("Total Stake: " + cashInHand);
-                    break;
+                switch (gamble) {
+                    case 1:
+                        System.out.println("You have won");
+                        cashInHand = cashInHand + everygameBet;
+                        System.out.println("Total Stake: " +cashInHand);
+                        break;
 
-                case 0:
-                    System.out.println("You have lost");
-                    cashInHand = cashInHand - everygameBet;
-                    System.out.println("Total Stake: " + cashInHand);
-                    break;
+                    case 0:
+                        System.out.println("You have lost");
+                        cashInHand = cashInHand - everygameBet;
+                        System.out.println("Total Stake: " + cashInHand);
+                        break;
 
-                default:
-                    System.out.println("Play again");
+                    default:
+                        System.out.println("Play again");
+                }
+                if (gamble == 1){
+                    System.out.println("Player has won");
+                } else {
+                    System.out.println("Player has lost");
+                }
             }
-            if (gamble == 1) {
-                System.out.println("Player has won");
-            } else {
-                System.out.println("Player has lost");
+            if (cashInHand == 50 || cashInHand == 150){
+                System.out.println("Player has resigned for the day "+i);
             }
+            finalAmount = finalAmount + cashInHand;
         }
+        System.out.println("Total amount won after 20 days: "+finalAmount);
     }
 }
-
